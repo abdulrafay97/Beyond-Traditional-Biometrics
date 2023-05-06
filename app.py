72453a70419d15aa6092ac38b04a10d907873a2c
Abdul Rafay <80894764+abdulrafay97@users.noreply.github.com>
2023-05-06 22:26:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="Image Comparison", page_icon=":eyes:", layout="wide")
+st.set_page_config(page_title="Image Comparison", page_icon=":lips:", layout="wide")
 
 # Add a title to the app
 st.title("Image Comparison")
